5c0b08202e10855903e0c88e566a9e612bb61c1e
Nitesh Jain <niteshjainy>
2023-08-19 20:21:59 +0530

    console error removes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import "./App.css";
 import Header from "./components/Header";
 import MovieCard from "./components/MovieCard/MovieCard";
-import { useEffect } from "react";
-import { setMovieList } from "./app/features/movie/movieSlice";
 
 function App() {
   const movieList = useSelector((state) => state.movie.movieList);
-  const dispatch = useDispatch();
 
   return (
     <main className="container-fluid">
       <Header />
       <div className="movie-container">
         {movieList?.length &&
-          movieList.map((movie) => <MovieCard movie={movie} />)}
+          movieList.map((item) => <MovieCard key={item.id} movie={item} />)}
       </div>
     </main>
   );
