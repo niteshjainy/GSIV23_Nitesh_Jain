33cd0a192d0cce573d6c4c3087f0c1e232bc0955
Nitesh Jain <niteshjainy>
2023-08-22 21:18:13 +0530

    auth setup ++

--- src/api.js
+++ src/api.js
@@ -1,12 +1,10 @@
 import axios from "axios";
-const token =
-  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5NjI2YjkzZTcyODBjZTZiY2JmMzc2YWZlMWU2ODU1NiIsInN1YiI6IjY0ZTBhNDUyMzcxMDk3MDBmZmJhMzgwZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.z70PBR3ABtekpk8YaRSCBkd8IQde3yGC35ipV6eAb58";
 
 const api = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   timeout: 3000,
   headers: {
-    Authorization: `Bearer ${token}`,
+    Authorization: `Bearer ${process.env.Auth_Token}`,
     accept: "application/json",
   },
 });
